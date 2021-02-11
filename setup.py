4de4726eb48019aeab8f56eac5f5a7be566c4365
r0w4n <6553343+r0w4n@users.noreply.github.com>
2021-02-11 21:04:48 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="edr query parser",
-    version="2.1",
+    version="2.2",
     author="r0w4n",
     author_email="r0w4n@nuisance.com",
     description="Environmental data retrieval API query parser",
